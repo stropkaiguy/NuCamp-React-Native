3d5964b0bfe9cc1c865138be19978a4485edcfda
NickCarothers <ncstropkai@gmail.com>
2022-10-18 19:26:26 -0800

    Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -11,6 +11,7 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
 
 const ReservationScreen = () => {
   const [campers, setCampers] = useState(1);
@@ -25,27 +26,34 @@ const ReservationScreen = () => {
   };
 
   const handleReservation = () => {
-    console.log("campers:", campers);
-    console.log("hikeIn:", hikeIn);
-    console.log("date:", date);
-
+    const message = `Number of Campers: ${campers}
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString("en-US")}`;
     Alert.alert(
       "Begin Search?",
-      `Number of Campers: ${campers}
-      \nHike In? ${hikeIn}
-      \nDate: ${date.toLocaleDateString("en-US")}`,
+      message,
       [
         {
           text: "Cancel",
-          onPress: () => console.log("Cancelled"),
+          onPress: () => {
+            console.log("Reservation Search Canceled");
+            resetForm();
+          },
           style: "cancel",
         },
         {
           text: "OK",
+          onPress: () => {
+            presentLocalNotification(date.toLocaleDateString("en-US"));
+            resetForm();
+          },
         },
       ],
       { cancelable: false }
     );
+    console.log("campers:", campers);
+    console.log("hikeIn:", hikeIn);
+    console.log("date:", date);
   };
 
   const resetForm = () => {
@@ -53,6 +61,34 @@ const ReservationScreen = () => {
     setHikeIn(false);
     setDate(new Date());
     setShowCalendar(false);
+  };
+
+  const presentLocalNotification = async (reservationDate) => {
+    const sendNotification = () => {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: true,
+        }),
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Your Campsite Reservation Search",
+          body: `Search for ${reservationDate} requested`,
+        },
+        trigger: null,
+      });
+    };
+
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
   };
 
   return (
@@ -128,22 +164,6 @@ const styles = StyleSheet.create({
   formItem: {
     flex: 1,
   },
-  modal: {
-    justifyContent: "center",
-    margin: 20,
-  },
-  modalTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    backgroundColor: "#5637DD",
-    textAlign: "center",
-    color: "#fff",
-    marginBottom: 20,
-  },
-  modalText: {
-    fontSize: 18,
-    margin: 10,
-  },
 });
-// test comment
+
 export default ReservationScreen;
